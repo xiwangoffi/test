0b0fe737e763c07a2ebe0128a4db1fedf122ecd7
Xiwang <thegostdev@outlook.com>
2023-03-16 18:31:08 +0100

    ajout de commentaire

--- src/app/formulaire-joueur/formulaire-joueur.component.ts
+++ src/app/formulaire-joueur/formulaire-joueur.component.ts
@@ -3,18 +3,18 @@ import { NgForm } from '@angular/forms';
 import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-formulaire-joueur',
-  templateUrl: './formulaire-joueur.component.html',
-  styleUrls: ['./formulaire-joueur.component.scss']
+  selector: 'app-formulaire-joueur', //Selection du fichier où se trouve la vue
+  templateUrl: './formulaire-joueur.component.html', // Selection du composant html
+  styleUrls: ['./formulaire-joueur.component.scss'] // Selection du fichier scss du composant html
 })
 
 export class FormulaireJoueurComponent implements OnInit {
   // Création de myData qui permet de récupérer les données du formulaires des joueurs.
-  myData: any[] | undefined;
+  myData: any[] | undefined; // Initialisation de la variable "myData" pour stocker les pseudos des joueurs
   
   constructor(private router:Router){}
   versPartie(){
-    this.router.navigate(['/debut-partie']);
+    this.router.navigate(['/debut-partie']); // Redirection des joueurs vers le début partie
   }
   @Output()
   sendRequestToFather = new EventEmitter();
@@ -22,8 +22,8 @@ export class FormulaireJoueurComponent implements OnInit {
   }
   // A la complétion du formulaire des joueurs, on envoit les noms dans Joueurs.
   onSubmit(form: NgForm) {
-    this.myData = [form.value.Joueur1, form.value.Joueur2];
-    console.log(this.myData);
+    this.myData = [form.value.Joueur1, form.value.Joueur2]; // Récupération des pseudos insérer dans le formulaire joueur
+    console.log(this.myData); // Affichage des pseudos en console pour debug
     // On envoit Joueurs pour traitement
     this.sendRequestToFather.emit(this.myData);
   }
